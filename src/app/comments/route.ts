44e4641fc758d9ad0d57e1f57a12fb1e26ac04ed
SamsulAlomLaskar <itsmesamsulalom@gmail.com>
2025-08-18 12:51:12 +0530

    post method

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -4,4 +4,21 @@ const GET = async () => {
   return Response.json(comments);
 };
 
-export { GET };
+const POST = async (request: Request) => {
+  const newComment = await request.json();
+
+  const createNewComment = {
+    id: comments.length + 1,
+    text: newComment.text,
+  };
+
+  comments.push(createNewComment);
+
+  return new Response(JSON.stringify(createNewComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+};
+export { GET, POST };
