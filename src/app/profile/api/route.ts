61078116d08b08a6be95746e6344d933688b2eb4
SamsulAlomLaskar <itsmesamsulalom@gmail.com>
2025-08-19 16:55:39 +0530

    cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,10 +1,19 @@
 import { NextRequest } from "next/server"; // method 1 to import NextRequest Headers
-import { headers } from "next/headers"; // method 2 to import NextRequest Headers
+import { headers, cookies } from "next/headers"; // method 2 to import NextRequest Headers
 
 const GET = async (request: NextRequest) => {
   const requestHeaders = new Headers(request.headers);
-  const nextHeaders = headers(); // method 2 to get headers
   console.log(requestHeaders.get("Authorization"), "Authorization Header");
+  const nextHeaders = headers(); // method 2 to get headers
+
+  const theme = request.cookies.get("theme"); // method 1 to get cookies
+
+  console.log(theme, "Cookie from request");
+
+  cookies().set("result", "success"); // method 2 to set cookies
+
+  console.log(cookies().get("result"), "Cookie from next/headers");
+
   console.log(
     nextHeaders.get("Authorization"),
     "Authorization Header from next/headers"
@@ -12,6 +21,7 @@ const GET = async (request: NextRequest) => {
   return new Response("<h1>Profile API Route</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark, sessionId=12345",
     },
   });
 };
