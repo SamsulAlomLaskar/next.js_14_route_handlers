a8fc9a2906855e3f992d47b369a3c4edb2db8610
SamsulAlomLaskar <itsmesamsulalom@gmail.com>
2025-08-19 16:26:58 +0530

    Headers in route handler

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,19 @@
-const GET = async () => {
-  return new Response("Profile API Route");
+import { NextRequest } from "next/server"; // method 1 to import NextRequest Headers
+import { headers } from "next/headers"; // method 2 to import NextRequest Headers
+
+const GET = async (request: NextRequest) => {
+  const requestHeaders = new Headers(request.headers);
+  const nextHeaders = headers(); // method 2 to get headers
+  console.log(requestHeaders.get("Authorization"), "Authorization Header");
+  console.log(
+    nextHeaders.get("Authorization"),
+    "Authorization Header from next/headers"
+  );
+  return new Response("<h1>Profile API Route</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 };
 
 export { GET };
